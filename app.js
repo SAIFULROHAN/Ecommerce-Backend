1a3bcb42615b52eac6e3a1a686fd674b340cfef9
SAIFULROHAN <141867050+SAIFULROHAN@users.noreply.github.com>
2024-11-26 20:55:48 +0600

    Project Start Folder

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import rateLimit from "express-rate-limit";
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import {DATABASE, MAX_JSON_SIZE, PORT, REQUEST_NUMBER, REQUEST_TIME, URL_ENCODE,WEB_CACHE} from "./app/config/config.js";
+import router from "./routes/api.js";
+
+const app = express();
+
+// App Use Default Middleware
+
+app.use(cors());
+app.use(express.json({limit:MAX_JSON_SIZE}));
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet())
+
+// App Use Limiter
+
+const limiter = rateLimit({windowMs:REQUEST_TIME,max:REQUEST_NUMBER})
+app.use(limiter)
+
+// Cache
+app.set('etag', WEB_CACHE)
+
+// Database Connect
+mongoose.connect(DATABASE,{autoIndex:true}).then(()=>{
+    console.log("Database Connected!");
+})
+.catch((err)=>{
+    console.log("Database Not Connected!");
+})
+
+app.use("/api",router)
+
+app.listen(PORT, () => {
+    console.log("Server running on port:" +PORT);
+})
